e7479d9e0f09025aaed9147631184fa333bd6c78
李文科 <wenke.li@dianrong.com>
2016-02-25 18:43:23 +0800

    Initial commit

---
+++ src/main/java/com/lwk/springboot/conf/WebMvcConfig.java
@@ -0,0 +1,55 @@
+package com.lwk.springboot.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+/**
+ * 该配置类 类似于mvc配置文件:spring_mvc.xml
+// @EnableWebMvc 用来导入mvc框架的自动化配置，使用前提是该类有@Configuration存在
+/*@ComponentScan 扫描控制器组件，使用方式有两种:
+* 1.指定具体类 例如@ComponentScan(basePackageClasses = HelloController.class)，或者 basePackageClasses = {HelloController.class,xxx.class,…}
+* 2.指定基础包 例如本例使用的方法，或者数组形式@ComponentScan({"com.*.**","com.*.**"})
+* 注：basePackages 关键字 可以选择性添加，默认会自动匹配到basePackages
+* */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.lwk.springboot.controller")
+public class WebMvcConfig {
+
+    //    声明页面的编码格式、类型
+    private static final String CONTENTTYPE = "text/html; charset=UTF-8";
+
+    //    Thymeleaf框架配置
+    @Bean
+    public FreeMarkerViewResolver templateResolver(){
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        resolver.setCache(false);//     去除缓存
+        resolver.setContentType(CONTENTTYPE);
+        return resolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer getFreeMarkerConfigurer(){
+        FreeMarkerConfigurer freeMarkerConfigurer=new FreeMarkerConfigurer();
+        freeMarkerConfigurer.setTemplateLoaderPath("/WEB-INF/views/");
+        return freeMarkerConfigurer;
+    }
+
+
+    /**
+     * 模板引擎解释器
+     * @return
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+
+        return templateResolver();
+    }
+}
---
+++ src/main/java/com/lwk/springboot/controller/HelloController.java
@@ -0,0 +1,41 @@
+package com.lwk.springboot.controller;
+
+import com.lwk.springboot.domain.User;
+import com.lwk.springboot.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+// 声明为controller控制器，捕获请求并处理请求
+@Controller
+public class HelloController {
+
+//    注入UserDao
+    @Autowired
+    private UserDao userDao;
+
+//    捕获 /hello 请求，利用hello(Model model) 处理请求，并返回‘hello’，交给Thymeleaf 处理
+//    hello 对应html文件名。model 作为信息的载体，封装各类变量、对象
+    @RequestMapping("/hello")
+    public ModelAndView hello(ModelAndView model) {
+
+//        获取XiaoMing的相关信息
+        User user = userDao.getOneUser("1");
+
+        model.addObject("user", user);
+        model.setViewName("hello");
+        return model;
+    }
+
+    @RequestMapping("/mm")
+    @ResponseBody
+    public Object mamadan(){
+        User user=new User();
+        user.setName("sfsf");
+        user.setAge(2323);
+        return user;
+    }
+}
